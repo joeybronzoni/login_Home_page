33fab1070eb47216d949fad2efaa67b508eb9d89
josephskrzysowski <josephskrzysowski@gmail.com>
2017-10-03 09:30:05 -0400

    all hooked up

--- footer.php
+++ footer.php
@@ -1,3 +1,20 @@
 
+ <div id="data">
+        <h4>Here is your geolocation baby: </h4>
+    </div>
+
+
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"> </script>
+        <script>
+        //for geolocation say yes to browser
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition((position)=>{
+                    $("#data").html("latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longi \
+                                    tude);
+                });
+        };
+
+</script>
 </body>
 </html>
